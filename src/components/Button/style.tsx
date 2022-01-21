dc6f2882fe45dcbc2e96bdc911664fdb52e337e4
Quinton Chester <quintonchester@gmail.com>
2022-01-20 23:50:06 -0600

    :sparkles: handle game completion
    Added logic in to handle when the user guesses the word properly. Also addressed some animation stuff. Note that when you pull this PR to review it you will have to install pods for some new deps.

---
+++ src/components/Button/style.tsx
@@ -0,0 +1,22 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  button: {
+    marginHorizontal: 7,
+    paddingVertical: 12,
+    paddingHorizontal: 64,
+    borderRadius: 6,
+    position: 'relative',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  icon: {
+    marginRight: 12,
+  },
+  title: {
+    textAlign: 'center',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+});
